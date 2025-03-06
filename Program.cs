308a04d3e4caefcf02b9f5c15c5f501266989ee9
Marcelo <120042864+EMarceloCM@users.noreply.github.com>
2025-03-06 16:35:55 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -51,23 +51,121 @@ stopwatch.Start();
 //GeneticAlgorithm geneticAlgorithm = new(instanceData);
 //var geneticResult = geneticAlgorithm.Solve();
 // Ant Colony Optimization
- AntColonyOptimization antColony = new(instanceData);
- var antResult = antColony.FindSolution();
+//AntColonyOptimization antColony = new(instanceData);
+//var antResult = antColony.FindSolution();
 // Tabu Search
 //TabuSearch tabuSearch = new(instanceData);
 //var tabuResult = tabuSearch.Solve();
 // ALNS
 // ALNS alns = new();
 // var alnsResult = alns.AdaptiveLargeNeighborhoodSearch(instanceData);
+
+List<List<int>> savingsSolution = new List<List<int>>
+{
+    new List<int> { 0, 4, 11, 8, 28, 9, 22, 18, 3, 23, 10, 29, 0 },
+    new List<int> { 0, 2, 17, 19, 15, 31, 14, 26, 0 },
+    new List<int> { 0, 25, 21, 5, 6, 13, 7, 20, 0 },
+    new List<int> { 0, 1, 12, 27, 16, 30, 0 },
+    new List<int> { 0, 24, 0 }
+};
+
+List<List<int>> insertionSolution = new List<List<int>>
+{
+    new List<int> { 0, 14, 6, 3, 28, 4, 11, 8, 18, 24, 0 },
+    new List<int> { 0, 26, 27, 23, 22, 9, 15, 10, 29, 5, 20, 0 },
+    new List<int> { 0, 30, 16, 7, 13, 21, 2, 0 },
+    new List<int> { 0, 12, 1, 31, 19, 25, 0 },
+    new List<int> { 0, 17, 0 }
+};
+
+List<List<int>> nearestNeighborSolution = new List<List<int>>
+{
+    new List<int> { 0, 30, 26, 16, 12, 1, 7, 14, 29, 22, 18, 0 },
+    new List<int> { 0, 24, 27, 20, 5, 25, 10, 8, 0 },
+    new List<int> { 0, 13, 21, 31, 19, 17, 3, 23, 0 },
+    new List<int> { 0, 6, 2, 28, 4, 11, 9, 0 },
+    new List<int> { 0, 15, 0 }
+};
+
+List<List<int>> geneticV1Solution = new List<List<int>>
+{
+    new List<int> { 0, 22, 4, 11, 12, 28, 24, 0 },
+    new List<int> { 0, 16, 1, 31, 19, 17, 3, 26, 0 },
+    new List<int> { 0, 21, 7, 14, 27, 29, 18, 2, 6, 20, 0 },
+    new List<int> { 0, 5, 25, 10, 15, 9, 8, 23, 0 },
+    new List<int> { 0, 13, 30, 0 }
+};
+
+List<List<int>> geneticV2Solution = new List<List<int>>
+{
+    new List<int> { 0, 22, 9, 11, 28, 4, 13, 0 },
+    new List<int> { 0, 26, 31, 19, 17, 2, 3, 23, 8, 18, 14, 0 },
+    new List<int> { 0, 6, 21, 7, 1, 12, 16, 0 },
+    new List<int> { 0, 30, 5, 29, 15, 10, 25, 20, 0 },
+    new List<int> { 0, 27, 24, 0 }
+};
+
+List<List<int>> antColonyV1Solution = new List<List<int>>
+{
+    new List<int> { 0, 20, 5, 25, 10, 15, 29, 22, 9, 8, 18, 0 },
+    new List<int> { 0, 30, 26, 16, 7, 1, 12, 0 },
+    new List<int> { 0, 13, 31, 19, 17, 21, 0 },
+    new List<int> { 0, 6, 3, 23, 2, 11, 4, 28, 0 },
+    new List<int> { 0, 27, 24, 14, 0 }
+};
+
+List<List<int>> antColonyV2Solution = new List<List<int>>
+{
+    new List<int> { 0, 30, 26, 16, 12, 1, 7, 0 },
+    new List<int> { 0, 19, 17, 31, 21, 13, 0 },
+    new List<int> { 0, 6, 3, 2, 23, 28, 4, 11, 0 },
+    new List<int> { 0, 20, 5, 25, 10, 29, 15, 9, 22, 18, 8, 0 },
+    new List<int> { 0, 14, 24, 27, 0 }
+};
+
 // Local Search
-// LocalSearch localSearch = new();
-//localSearch.ImproveSolution(instanceData, savingsResult, initialSolutionAlgorithm: "Savings");
-//localSearch.ImproveSolution(instanceData, insertionResult, initialSolutionAlgorithm: "Insertion");
-//localSearch.ImproveSolution(instanceData, nearestResult, initialSolutionAlgorithm: "Nearest Neighbor");
-//localSearch.ImproveSolution(instanceData, geneticResult, initialSolutionAlgorithm: "Genetic Algorithm");
-//localSearch.ImproveSolution(instanceData, antResult!, initialSolutionAlgorithm: "Ant Colony Optmization");
-//localSearch.ImproveSolution(instanceData, tabuResult, initialSolutionAlgorithm: "Tabu Search");
-//localSearch.ImproveSolution(instanceData, alnsResult, initialSolutionAlgorithm: "ALNS");
+LocalSearch localSearch = new();
+var savingsResult = localSearch.ImproveSolution(instanceData, savingsSolution, "Savings");
+localSearch.PrintSolution(savingsResult, instanceData);
+stopwatch.Stop();
+Console.WriteLine("Tempo de execução: " + stopwatch.ElapsedMilliseconds + " ms");
+/*
+stopwatch.Restart();
+var insertionResult = localSearch.ImproveSolution(instanceData, insertionSolution, "Insertion");
+localSearch.PrintSolution(insertionResult, instanceData);
+stopwatch.Stop();
+Console.WriteLine("Tempo de execução: " + stopwatch.ElapsedMilliseconds + " ms");
+
+stopwatch.Restart();
+var nearestResult = localSearch.ImproveSolution(instanceData, nearestNeighborSolution, "Nearest Neighbor");
+localSearch.PrintSolution(nearestResult, instanceData);
+stopwatch.Stop();
+Console.WriteLine("Tempo de execução: " + stopwatch.ElapsedMilliseconds + " ms");
+
+stopwatch.Restart();
+var geneticV1Result = localSearch.ImproveSolution(instanceData, geneticV1Solution, "Genetic Algorithm");
+localSearch.PrintSolution(geneticV1Result, instanceData);
+stopwatch.Stop();
+Console.WriteLine("Tempo de execução: " + stopwatch.ElapsedMilliseconds + " ms");
+
+stopwatch.Restart();
+var geneticV2Result = localSearch.ImproveSolution(instanceData, geneticV2Solution, "Genetic Algorithm");
+localSearch.PrintSolution(geneticV2Result, instanceData);
+stopwatch.Stop();
+Console.WriteLine("Tempo de execução: " + stopwatch.ElapsedMilliseconds + " ms");
+
+stopwatch.Restart();
+var antColonyV1Result = localSearch.ImproveSolution(instanceData, antColonyV1Solution, "Ant Colony");
+localSearch.PrintSolution(antColonyV1Result, instanceData);
+stopwatch.Stop();
+Console.WriteLine("Tempo de execução: " + stopwatch.ElapsedMilliseconds + " ms");
+
+stopwatch.Restart();
+var antColonyV2Result = localSearch.ImproveSolution(instanceData, antColonyV2Solution, "Ant Colony");
+localSearch.PrintSolution(antColonyV2Result, instanceData);
+stopwatch.Stop();
+Console.WriteLine("Tempo de execução: " + stopwatch.ElapsedMilliseconds + " ms");
+*/
 
 // Grasp
 // Grasp grasp = new();
